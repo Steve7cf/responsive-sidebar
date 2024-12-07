dbe871ea5213d94ea31f6fc3c0f04320cd7af965
steve bazaar <stevebazaar99@gmail.com>
2024-12-07 11:03:00 +0300

    main

--- main.js
+++ main.js
@@ -1,39 +1,8 @@
-// creating a custom html element ~ web components
+// toggle side bar
 
-const template = document.createElement("template")
-template.innerHTML = `
-<style>
-    .navbar{
-        background-color:#ebeef1;
-        display:flex;
-        justify-content:space-evenly;
-        align-items:center;
-        border-radius:1rem;
-        box-shadow:0px 0px 10px rgba(0,0,0,0.16);
-        height:100px;
-        padding:0.4rem;
-        width:100%;
-    }
-</style>
-<nav class="navbar">
-    <slot name="logo">logo</slot>
-    <slot name="links">links</slot>
-    <slot name="btn">buttons</slot>
-</nav>
-`
+const sidebar = document.querySelector('.sidebar')
+const toggle = document.querySelector('.toggler')
 
-// autonomous web components
-class customElement extends HTMLElement{
-    constructor(){
-        super()
-        const shadowRoot = this.attachShadow({mode:"open"})
-        const clone = template.content.cloneNode(true)
-        console.log(clone.firstChild)
-        shadowRoot.append(clone)
-
-    }
-}
-
-customElements.define("custom-element", customElement)
-
-// stephan bazaar
+toggle.addEventListener('click', () => {
+    sidebar.classList.toggle("collapsed")
+})
